ccb0ce9e60550a819732eca66b2326d53c24c0a8
Kacper <kkdeverer@outlook.com>
2025-11-27 13:59:14 +0100

    .add basic model classes

---
+++ Models/Domain.cs
@@ -0,0 +1,9 @@
+﻿namespace ExamMeAI.Models
+{
+    public class Domain
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,13 @@
+﻿namespace ExamMeAI.Models
+{
+    public class Question
+    {
+
+        public int Id { get; set; }
+        public string text { get; set; }    
+
+        public Genre genre_id { get; set; }
+
+        public Domain domain_id { get; set; }
+    }
+}
