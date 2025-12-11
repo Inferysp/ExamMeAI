f2d3033c60fba5948a7ec24e4072fcfb574580be
Kacper <kkdeverer@outlook.com>
2025-12-11 15:53:39 +0100

    .add VC for question listing page

---
+++ Controllers/ExamController.cs
@@ -0,0 +1,29 @@
+﻿using ExamMeAI.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ExamMeAI.Controllers
+{
+    public class ExamController : Controller
+    {
+        private readonly ExamMeAiContext _context;
+
+        public ExamController(ExamMeAiContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ExamMe()
+        {
+            return View();
+        }
+
+        // Akcja zwracająca listę pytań     
+        public IActionResult GetQuestions()
+        {
+            var questions = _context.Question.ToList();
+            return PartialView("ExamMe", questions);
+        }
+    }
+}
