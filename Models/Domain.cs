3fc1a8e7ab55e9157b8dadb20342622be4eab968
Kacper <kkdeverer@outlook.com>
2025-12-17 19:21:41 +0100

    .add Domain Controll View by scaffolding

--- Models/Domain.cs
+++ Models/Domain.cs
@@ -1,9 +1,13 @@
-﻿namespace ExamMeAI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamMeAI.Models
 {
     public class Domain
     {
+        [Key]
         public int ID { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public ICollection<Question> Questions { get; set; }
