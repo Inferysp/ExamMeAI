2dd7e152131aa5ae4a8151398c1393cf20110d53
Kacper <kkdeverer@outlook.com>
2025-12-18 11:54:55 +0100

    .add Domain property non nullable constructor warning supress

--- Models/Domain.cs
+++ Models/Domain.cs
@@ -4,13 +4,11 @@ namespace ExamMeAI.Models
 {
     public class Domain
     {
-        [Key]
         public int ID { get; set; }
 
-        [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
 
-        public ICollection<Question> Questions { get; set; }
+        public ICollection<Question> Questions { get; set; } = null!;
 
     }
 }
