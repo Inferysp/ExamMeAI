559168b35cec10fa4eaab555098fec5430c3b09c
Kacper <kkdeverer@outlook.com>
2025-12-19 11:26:36 +0100

    .addd DemoPage for development tests

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,19 @@
+﻿using ExamMeAI.Demo.OpenAI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamMeAI.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult DemoPage()
+        {
+            return View();
+        }
+
+        public IActionResult MakeTest()
+        {
+            ChatDemo.GetInstance().WriteAnswerToConsole();
+            return View();
+        }
+    }
+}
---
+++ Demo/OpenAI/ChatDemo.cs
@@ -0,0 +1,27 @@
+﻿using OpenAI.Chat;
+
+namespace ExamMeAI.Demo.OpenAI
+{
+    public sealed class ChatDemo
+    {
+        private static ChatDemo _chatDemo = null!;
+
+        public static ChatDemo GetInstance()
+        {
+            if(_chatDemo == null)
+            {
+                return new ChatDemo();
+            }
+            return _chatDemo;
+        }
+
+        internal void WriteAnswerToConsole()
+        {
+            ChatClient client = new(model: "gpt-4o", apiKey: "your-api");
+
+            ChatCompletion completion = client.CompleteChat("Jak używać delegató we wzorcu projektowym DI?");
+
+            Console.WriteLine($"OpenAI response: {completion.Content[0].Text}");
+        }
+    }
+}
