02e063c6fbebbc5e82a26d79a4b7733541e52d04
Kacper <kkdeverer@outlook.com>
2025-12-20 09:25:31 +0100

    .dev open/close principle controller requests separate, OpenAI request on btn click with AJAX post request

--- Controllers/DemoController.cs
+++ Controllers/DemoController.cs
@@ -4,6 +4,7 @@ using Azure.Identity;
 using ExamMeAI.Demo.OpenAI;
 using ExamMeAI.singletons;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using OpenAI;
 using OpenAI.Chat;
 
@@ -27,28 +28,43 @@ namespace ExamMeAI.Controllers
         [HttpPost]
         public IActionResult PostOpenAiResponceOnBtnClick()
         {
-            //var client = new OpenAIClient(new AzureKeyCredential(_config.GetConnectionString("OpenAIKey")));
+            var client = new OpenAIClient(new AzureKeyCredential(_config.GetConnectionString("OpenAIKey")));
 
-            //ChatClient chatClient = client.GetChatClient("gpt-5-nano");
+            ChatClient chatClient = client.GetChatClient("gpt-5-nano");
 
 
-            //ChatCompletion completion = chatClient.CompleteChat(
-            //    [
-            //        // System messages represent instructions or other guidance about how the assistant should behave
-            //        new SystemChatMessage("You are a helpful assistant that talks like a Miki mouse."),
-            //        new UserChatMessage("Jak używać asynchroniczności we wzorcu projektowym DI??"),
-            //    ]);
+            ChatCompletion completion = chatClient.CompleteChat(
+                [
+                    // System messages represent instructions or other guidance about how the assistant should behave
+                    new SystemChatMessage("You are a helpful assistant that talks like a Miki mouse."),
+                    new UserChatMessage("Jak używać asynchroniczności we wzorcu projektowym DI??"),
+                ]);
 
-            //ViewData["response"] = $"{completion.Role}: {completion.Content[0].Text}";
+            var r = $"{completion.Role}: {completion.Content[0].Text}";
+
+            Console.WriteLine($"testy stringa: {r}");
+            return Content(r);
+        }
+
+        //Akcja Dla AJAX
+        [HttpPost]
+        public IActionResult PostStringOnBtnClick()
+        {
             var r = "testowy massage Demo!!!";
+            Console.WriteLine($"testy stringa: {r}");
+            return Content(r);
+        }
 
+        //Akcja Dla AJAX
+        [HttpPost]
+        public async Task<IActionResult> PostOpenRouterResponseOnBtnClick()
+        {
             if (false)
             {
                 if (_config.GetConnectionString("OpenRouterKey") != null)
                     ChatDemo.GetInstance().WriteAnswerToConsole(_config.GetConnectionString("OpenRouterKey"));
             }
-            Console.WriteLine($"testy stringa: {r}");
-            return Content(r);
+            return View();
         }
     }
 }
