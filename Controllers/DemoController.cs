f5f7da5e5413db932e4c20f196e6dc29b5a039b8
Kacper <kkdeverer@outlook.com>
2025-12-20 09:42:46 +0100

    .dev OpenAI responce comment, html atributes mess up, AJAX fetch routing with @Url.Action http path

--- Controllers/DemoController.cs
+++ Controllers/DemoController.cs
@@ -28,22 +28,22 @@ namespace ExamMeAI.Controllers
         [HttpPost]
         public IActionResult PostOpenAiResponceOnBtnClick()
         {
-            var client = new OpenAIClient(new AzureKeyCredential(_config.GetConnectionString("OpenAIKey")));
+            //var client = new OpenAIClient(new AzureKeyCredential(_config.GetConnectionString("OpenAIKey")));
 
-            ChatClient chatClient = client.GetChatClient("gpt-5-nano");
+            //ChatClient chatClient = client.GetChatClient("gpt-5-nano");
 
 
-            ChatCompletion completion = chatClient.CompleteChat(
-                [
-                    // System messages represent instructions or other guidance about how the assistant should behave
-                    new SystemChatMessage("You are a helpful assistant that talks like a Miki mouse."),
-                    new UserChatMessage("Jak używać asynchroniczności we wzorcu projektowym DI??"),
-                ]);
+            //ChatCompletion completion = chatClient.CompleteChat(
+            //    [
+            //        // System messages represent instructions or other guidance about how the assistant should behave
+            //        new SystemChatMessage("You are a helpful assistant that talks like a Miki mouse."),
+            //        new UserChatMessage("Jak używać asynchroniczności we wzorcu projektowym DI??"),
+            //    ]);
 
-            var r = $"{completion.Role}: {completion.Content[0].Text}";
+            //var r = $"{completion.Role}: {completion.Content[0].Text}";
 
-            Console.WriteLine($"testy stringa: {r}");
-            return Content(r);
+            //Console.WriteLine($"testy stringa: {r}");
+            return Content("test");
         }
 
         //Akcja Dla AJAX
